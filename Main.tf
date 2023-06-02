3a1c1339e55a5b0d794b28775eaaa46a59e99897
Jen Xu <135261703+shcsd-jenxu@users.noreply.github.com>
2023-06-01 21:32:17 -0700

    Following alone the demo by Joe

---
+++ Main.tf
@@ -0,0 +1,24 @@
+terraform {
+            required_version = ">= 1.4.0, < 2.0.0"
+            
+            Backend "Azurerm" {}
+            required_providers {
+                azurerm = {
+                    source = "hashicorp/azurerm"
+                    version = "3.52.0, < 4.0.0"
+      
+                }
+             }
+}
+
+provider "azurerm" {
+  features {}
+
+}
+
+resource "azurerm_resource_group" "rg"{
+    name = "testingTerraform-rg"
+    location = "west US 3"
+
+
+}
