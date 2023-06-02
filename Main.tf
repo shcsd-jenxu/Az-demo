4e12420f725a8d81f28d11586725dfbd7bb94965
Jen Xu <135261703+shcsd-jenxu@users.noreply.github.com>
2023-06-01 22:11:25 -0700

    Update syntax error on Main.tf

--- Main.tf
+++ Main.tf
@@ -1,7 +1,7 @@
 terraform {
             required_version = ">= 1.4.0, < 2.0.0"
             
-            Backend "Azurerm" {}
+            backend "Azurerm" {}
             required_providers {
                 azurerm = {
                     source = "hashicorp/azurerm"
